bb53f63e12b4a9454c4fc06c89fb9c7b5d311a63
José Di Gregorio <josedgregorio07@gmail.com>
2025-07-31 17:50:13 -0600

    change interceptors

--- src/interceptors.ts
+++ src/interceptors.ts
@@ -4,6 +4,23 @@ const setupInterceptors = () => {
     axios.defaults.withCredentials = true
     axios.defaults.withXSRFToken = true
 
+    axios.interceptors.request.use(
+        (config) => {
+            if (config.url?.startsWith('http://')) {
+                config.url = config.url.replace('http://', 'https://')
+            }
+
+            if (config.baseURL?.startsWith('http://')) {
+                config.baseURL = config.baseURL.replace('http://', 'https://')
+            }
+
+            return config
+        },
+        (error) => {
+            return Promise.reject(error)
+        }
+    )
+
     axios.interceptors.response.use(
         (response) => {
             return response
